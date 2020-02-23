f7c783ee2dbd619ee8da9e47e26d7fb95adc7533
Maxim <financial_monster@mail.ru>
2020-02-23 18:25:48 +0400

    Video API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import style from './sass/style.scss';
-import video from './video/01-components.mp4';
+import video1 from './video/1.mp4';
+import video2 from './video/2.mp4';
 import './js/header.js';
 import './js/feedback.js';
 import './js/video.js';
--- src/js/header.js
+++ src/js/header.js
@@ -4,16 +4,12 @@ const menu = document.querySelector(`.header__list`);
 
 menu.classList.remove(`no-js`);
 
-closeBtn.addEventListener(`click`, function(e) {
-	menu.style.display = `none`;
-	menu.style.paddingBottom = `0px`;
-	openBtn.style.display = `block`;
-	e.target.style.display = `none`;
+closeBtn.addEventListener(`click`, function() {
+	menu.classList.add(`closedMenu`);
+	menu.classList.remove(`openedMenu`);
 });
 
-openBtn.addEventListener(`click`, function(e) {
-	menu.style.display = `block`;
-	menu.style.paddingBottom = `56px`;
-	closeBtn.style.display = `block`;
-	e.target.style.display = `none`;
+openBtn.addEventListener(`click`, function() {
+	menu.classList.add(`openedMenu`);
+	menu.classList.remove(`closedMenu`);
 });
